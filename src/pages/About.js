86283766ad61ed7b48fd997a8fc6a2c66eb22c66
ehmatthes <robertstipp17@gmail.com>
2022-01-11 20:40:11 -0800

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about page</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit est quos
+        aperiam debitis impedit beatae sed, culpa in minima mollitia nostrum,
+        laborum omnis quod minus esse veritatis atque dicta optio placeat
+        excepturi qui cupiditate sapiente necessitatibus! Ad ipsam magnam vero?
+      </p>
+    </section>
+  );
+};
+
+export default About;
